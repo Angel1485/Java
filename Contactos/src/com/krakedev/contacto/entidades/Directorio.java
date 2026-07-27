9c446e8c974c34b40f363c6fd28e63ea4d27294c
Angel Sarango <asarango@coopcrea.fin.ec>
2026-07-27 16:59:59 -0500

    Ultimos cambios del proyecto Contactos

--- Contactos/src/com/krakedev/contacto/entidades/Directorio.java
+++ Contactos/src/com/krakedev/contacto/entidades/Directorio.java
@@ -47,14 +47,30 @@ public class Directorio {
         return numero;
     }
 
+    // public Contacto buscarContacto(String numero) {
+    //     Contacto encontrado = null;
+    //     for(int i = 0; i < contactos.size(); i++) {
+    //         Contacto c = contactos.get(i);
+    //         if(c.getCelular().equals(numero)) {
+    //             encontrado =  c;
+    //             break;
+    //         }           
+    //     }
+    //     return encontrado;
+    // }
+
     public Contacto buscarContacto(String numero) {
-        for(int i = 0; i < contactos.size(); i++) {
-            Contacto c = contactos.get(i);
-            if(c.getCelular().equals(numero)) {
-                return c;
+
+        Contacto encontrado = null;
+        for (Contacto c : contactos) {
+            if(c.getCelular().equals(numero)){
+                encontrado = c;
+                break;
+            }else{
+
             }
         }
-        return null;
+        return encontrado;
     }
 
     public boolean eliminarContacto(String numero) {
---
+++ Contactos/src/com/krakedev/contacto/entidades/test/TestSinBreak.java
@@ -0,0 +1,38 @@
+package com.krakedev.contacto.entidades.test;
+
+import com.krakedev.contacto.entidades.Contacto;
+import com.krakedev.contacto.entidades.Directorio;
+
+public class TestSinBreak {
+
+    public static void main(String[] args) {
+        
+        Directorio dir = new Directorio();
+        Contacto c1 = new Contacto();
+        c1.setNombre("Maria");
+        c1.setCelular("0102");
+
+        Contacto c2 = new Contacto();
+        c2.setNombre("Mafer");
+        c2.setCelular("0304");
+
+        Contacto c3 = new Contacto();
+        c3.setNombre("Carlos");
+        c3.setCelular("0506");
+
+        Contacto c4 = new Contacto();
+        c4.setNombre("Pablo");
+        c4.setCelular("050605");
+
+        dir.agregarContacto(c1);
+        dir.agregarContacto(c2);
+        dir.agregarContacto(c3);
+        dir.agregarContacto(c4);
+
+        //"Contacto" es el nombre del Array
+        Contacto contactoEncontrado =  dir.buscarContacto("0506");
+
+        System.out.println("Nombre: " + contactoEncontrado.getNombre());
+    }
+
+}
